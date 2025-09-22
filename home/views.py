aa7a2607a5a945127f55f2b8a57dec5ee2fc5e91
Carlos Eduardo Dias Carneiro <carloseduardodiascarneiro2@gmail.com>
2025-09-22 09:47:09 -0300

    Implementação de contentblocks e  correção do css

--- home/views.py
+++ home/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'home/index.html')
+    # Detecta user agent para mobile
+    user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
+    is_mobile = any(device in user_agent for device in ['mobile', 'android', 'iphone'])
+    print(f"User Agent: {user_agent}, Is Mobile: {is_mobile}")
+    if is_mobile:
+        return render(request, 'home/index-mobile.html')
+    else:
+        return render(request, 'home/index-desktop.html')
